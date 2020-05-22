962290c598e717ae553dbb5f82a967b812f42aa6
yury.timoschuk <yury.timoschuk@gmail.com>
2020-05-22 17:27:56 +0300

    Parse input json in REST launcher

--- src/HTTP/REST.php
+++ src/HTTP/REST.php
@@ -8,13 +8,22 @@
  {
     public function process($target)
     {
-      // TODO: parse input and put to call as params
       /** @var core\Node $target */
+
+       // parse json input and put to call as params
+      try
+       {
+        $request = (array)json_decode($this->context->http()->request()->body(), true, 512, JSON_THROW_ON_ERROR);
+        $this->context->input()->link(new Common\Collection($request));
+       }
+      catch(\JsonException $e) { $request = null; }
+
+      // index by request type
       $index = $this->config->index;
       if($index instanceof Common\I\Collection)
         $index = $index[$this->server->request()->method()] ?? $index->default;
-      try { return json_encode($result = $target->call($index ?? self::DEFAULT_INDEX), JSON_THROW_ON_ERROR); }
-      catch(\JsonException $e) { $this->context->log()->error('Failed of encode REST response.', [$result]); }
+      try { return json_encode($result = $target->call($index ?? self::DEFAULT_INDEX, $request), JSON_THROW_ON_ERROR); }
+      catch(\JsonException $e) { $this->context->log()->error('Failed of encode REST response.', isset($result) ? [$result] : null); }
       return '{}';
     }
  }
