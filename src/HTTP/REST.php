76733aba418643f35c02ae644d9e488ee1cd36d5
yury.timoschuk <yury.timoschuk@gmail.com>
2020-05-25 14:42:29 +0300

    use any method of input parameters in REST request

--- src/HTTP/REST.php
+++ src/HTTP/REST.php
@@ -11,18 +11,27 @@
       /** @var core\Node $target */
 
        // parse json input and put to call as params
-      try
-       {
-        $request = (array)json_decode($this->context->http()->request()->body(), true, 512, JSON_THROW_ON_ERROR);
-        $this->context->input()->link(new Common\Collection($request));
-       }
-      catch(\JsonException $e) { $request = null; }
+      $request = null;
+      if($requestBody = $this->context->http()->request()->body())
+        try
+         {
+          $request = (array)json_decode($requestBody, true, 512, JSON_THROW_ON_ERROR);
+          $this->context->input()->link(new Common\Collection($request));
+         }
+        catch(\JsonException $e) {}
 
       // index by request type
       $index = $this->config->index;
       if($index instanceof Common\I\Collection)
         $index = $index[$this->server->request()->method()] ?? $index->default;
-      try { return json_encode($result = $target->call($index ?? self::DEFAULT_INDEX, $request), JSON_THROW_ON_ERROR); }
+      try
+       {
+        return json_encode($result = $target->call(
+            $index ?? self::DEFAULT_INDEX,
+            $request ?? $this->context->input()->array()
+        ), JSON_THROW_ON_ERROR);
+       }
+      catch(RestError $e) { return json_encode(['error' => $e->getMessage()], JSON_THROW_ON_ERROR); }
       catch(\JsonException $e) { $this->context->log()->error('Failed of encode REST response.', isset($result) ? [$result] : null); }
       return '{}';
     }
