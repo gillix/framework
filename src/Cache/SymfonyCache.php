a07f8a4adad64554a4efe2a9ffdef63da7d2abe6
yury.timoschuk <yury.timoschuk@gmail.com>
2021-02-11 22:01:08 +0200

    replace reserved symbols for symfony cache adapters

--- src/Cache/SymfonyCache.php
+++ src/Cache/SymfonyCache.php
@@ -15,16 +15,21 @@
         
         public function get(string $key)
         {
-            return $this->adapter->get($key);
+            return $this->adapter->get($this->normalizeKey($key));
         }
         
         public function store(string $key, $value, int $lifetime = 0): void
         {
-            $this->adapter->set($key, $value, $lifetime);
+            $this->adapter->set($this->normalizeKey($key), $value, $lifetime);
         }
         
         public function delete(string $key, bool $search = false): void
         {
-            $this->adapter->delete($key);
+            $this->adapter->delete($this->normalizeKey($key));
+        }
+        
+        protected function normalizeKey(string $key): string
+        {
+            return str_replace(['(', ')', '{', '}', ':', '@', '\\', '/'], ['[', ']', '[', ']', '.', '%', '|', '|'], $key);
         }
     }
