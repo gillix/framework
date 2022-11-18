7496b0c4fc42a8c5f8374f553e81ae94404288f4
Yury <yury.timoschuk@biletum.com>
2022-11-18 20:22:48 +0200

    fix of previous

--- src/I/Context.php
+++ src/I/Context.php
@@ -4,6 +4,7 @@
     
     use glx\Cache;
     use glx\Common;
+    use glx\Common\I\Collection;
     use glx\Context\I\CallStack;
     use glx\Context\I\Profile;
     use glx\HTTP;
@@ -14,9 +15,17 @@
         public function callstack(): CallStack;
         
         public function persistent(): Cache\I\Persistent;
-        
+
+        /**
+         * @param string|null $name
+         * @return Collection|mixed
+         */
         public function temporary(string $name = null): mixed;
-        
+
+        /**
+         * @param string|null $name
+         * @return Collection|mixed
+         */
         public function input(string $name = null): mixed;
         
         public function http(): HTTP\I\Server;
